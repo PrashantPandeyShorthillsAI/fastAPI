d43014c289be005208bcb1baba7482154ab16139
Prashant Pandey <prashant.pandey@shorthills.ai>
2023-12-06 12:16:37 +0530

    crud locally

--- crud2/main.py
+++ crud2/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello world!!!"}
+async def get_all_data():
+    return db
+
+# post 
+db={}
+
+class Item(BaseModel):
+    name:str
+    description: str
+
+@app.post("/")
+def create(item: Item):
+    db[item.name] = item.description
+    return {"item": item}
+
+@app.delete("/")
+def delete_data(name: str):
+    del db[name]
+    return db
+
+@app.put("/")
+def update_data(item: Item):
+    db[item.name] = item.description
+    return db
