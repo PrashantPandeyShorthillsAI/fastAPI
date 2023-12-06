8c93723a2370ec20ae6c3d8f52bb42f0c73007be
Prashant Pandey <prashant.pandey@shorthills.ai>
2023-12-06 12:38:58 +0530

    crud locally

--- crud2/models.py
+++
@@ -1,9 +0,0 @@
-# from db import Base
-# from sqlalchemy import Column, Integer, String
-
-# class Users(Base):
-#     __tablename__ = 'users'
-#     id=Column(Integer, primary_key=True,index=True)
-#     name=Column(String(50))
-#     email=Column(String(50),unique=True)
-#     password=Column(String(200))
